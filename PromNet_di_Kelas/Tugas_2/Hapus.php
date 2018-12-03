71c7ba348a15fbcce4a36ea0dc7e0b0bc43c05f1
IrfanKnowledge <45427562+IrfanKnowledge@users.noreply.github.com>
2018-12-03 20:11:11 +0700

    Delete

--- PromNet_di_Kelas/Tugas_2/Hapus.php
+++
@@ -1,8 +0,0 @@
-<?php 
-if (isset($_GET['nim'])) {
-	$temp = new angkatan();
-	$temp -> hapus($_GET['nim'], $_GET['indeks']);
-	echo "<meta http-equiv='refresh' content='0 url=index.php'>";
-	die();	//untuk menghentikan proses compile script berikutnya
-}
-?>
