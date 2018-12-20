7ba25b7421a4b4912b2f96ca985242b7a8d63fff
IrfanKnowledge <45427562+IrfanKnowledge@users.noreply.github.com>
2018-12-21 06:05:21 +0700

    Final

--- api/server/index.php
+++ api/server/index.php
@@ -3,23 +3,23 @@
 $connection=mysqli_connect('localhost', 'root', '', 'karyawan');
 
  $request_method=$_SERVER["REQUEST_METHOD"]; switch ($request_method) {
-   case 'GET':    // GET – mengambil informasi produk
-              if (!empty($_GET["product_id"])) {
-                  $product_id=intval($_GET["product_id"]);
-                  get_products($product_id);
+   case 'GET':    // GET – mengambil informasi semua karyawan atau karyawan tertentu
+              if (!empty($_GET["id"])) {
+                  $id=intval($_GET["id"]);
+                  get_products($id);
               } else {
                   get_products();
               }
               break;
-  case 'POST':    // POST – menambahkan produkl baru
+  case 'POST':    // POST – menambahkan karyawan baru
                   insert_product();
               break;
-  case 'PUT':    // POST – menambahkan produkl baru
-             $product_id=intval($_GET["product_id"]);    update_product($product_id);
+  case 'PUT':    // PUT - mengupdate karyawan tertentu 
+             $id=intval($_GET["id"]);    update_product($id);
              break;
 
-  case 'DELETE':    // DELETE – menghapus produk tertentu
-                $product_id=intval($_GET["product_id"]);    delete_product($product_id);
+  case 'DELETE':    // DELETE – menghapus karyawan tertentu
+                $id=intval($_GET["id"]);    delete_product($id);
                 break;
   default:    // metode request tidak valid (salah)
             header("HTTP/1.0 405 Metode Tidak Dikenali ");
@@ -46,13 +46,13 @@ $connection=mysqli_connect('localhost', 'root', '', 'karyawan');
      echo json_encode($response);
  }
 
- function get_products($product_id=0)
+ function get_products($id=0)
  {
      global $connection;
      $query="SELECT * FROM tb_karyawan";
 
-     if ($product_id != 0) {
-         $query="SELECT * FROM tb_karyawan WHERE id=" . "'$product_id'" . "LIMIT 1";
+     if ($id != 0) {
+         $query="SELECT * FROM tb_karyawan WHERE id=" . "'$id'" . "LIMIT 1";
      }
 
      $response=array();
@@ -65,10 +65,10 @@ $connection=mysqli_connect('localhost', 'root', '', 'karyawan');
      echo json_encode($response);
  }
 
- function delete_product($product_id=0)
+ function delete_product($id=0)
  {
      global $connection;
-     $query="DELETE FROM tb_karyawan WHERE id='$product_id'";
+     $query="DELETE FROM tb_karyawan WHERE id='$id'";
 
      if (mysqli_query($connection, $query)) {
          $response=array(     'status' => 1,     'status_message' => 'Karyawan Berhasil Dihapus.'    );
@@ -79,7 +79,7 @@ $connection=mysqli_connect('localhost', 'root', '', 'karyawan');
      echo json_encode($response);
  }
 
- function update_product($product_id=0)
+ function update_product($id=0)
  {
      global $connection;
      parse_str(file_get_contents("php://input"), $post_vars);
@@ -88,7 +88,7 @@ $connection=mysqli_connect('localhost', 'root', '', 'karyawan');
      $address=$post_vars["address"];
      $phone=$post_vars["phone"];
 
-     $query =  "UPDATE tb_karyawan SET name='$name', email='$email', address='$address', phone='$phone' WHERE id= '$product_id'";
+     $query =  "UPDATE tb_karyawan SET name='$name', email='$email', address='$address', phone='$phone' WHERE id= '$id'";
 
      if (mysqli_query($connection, $query)) {
          $response=array(     'status' => 1,     'status_message' =>' Karyawan Berhasil Diperbarui.'    );
