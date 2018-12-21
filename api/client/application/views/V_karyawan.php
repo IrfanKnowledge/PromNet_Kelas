0b61b183ca75859a895f3d0ce670dabbadbc648c
IrfanKnowledge <45427562+IrfanKnowledge@users.noreply.github.com>
2018-12-21 18:14:10 +0700

    Final

--- api/client/application/views/V_karyawan.php
+++ api/client/application/views/V_karyawan.php
@@ -4,7 +4,7 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>CRUD - Quiz Promnet</title>
+	<title>Web Service - PromNet Tgs_Ind_7</title>
 	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/font.css">
 	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/font-awesome.min.css">
 	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/bootstrap.min.css">
